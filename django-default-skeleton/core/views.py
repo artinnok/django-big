5cf1be38b45d1dedb3a3546530718902fb5b6bbe
Artem Innokentiev <artinnok@users.noreply.github.com>
2016-05-30 17:12:50 +0300

    views changed

--- django-default-skeleton/core/views.py
+++ django-default-skeleton/core/views.py
@@ -1,4 +1,9 @@
 from django.shortcuts import render
+from django.views.generic import TemplateView
+
+
+class IndexView(TemplateView):
+    template_name = 'core/index.html'
 
 
 def error404(request):
