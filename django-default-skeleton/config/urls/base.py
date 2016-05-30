52768a27ceaa0d57f3efebe7aefadb3bf3dad8ce
Artem Innokentiev <artinnok@users.noreply.github.com>
2016-05-30 17:12:28 +0300

    urls changed

--- django-default-skeleton/config/urls/base.py
+++ django-default-skeleton/config/urls/base.py
@@ -1,8 +1,15 @@
 from django.conf.urls import url
 from django.contrib import admin
-from django.views.generic import TemplateView
+
+from core.views import IndexView
 
 urlpatterns = [
-    url(r'^$', TemplateView.as_view(template_name='core/index.html')),
-    url(r'^admin/', admin.site.urls),
+    url(
+        regex=r'^$',
+        view=IndexView.as_view(),
+        name='index'
+    ),
+    url(
+        regex=r'^admin/',
+        view=admin.site.urls),
 ]
