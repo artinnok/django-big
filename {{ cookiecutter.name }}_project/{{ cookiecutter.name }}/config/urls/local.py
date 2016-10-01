984905c1edc21868462c0803cf4bb643082b878c
Artem Innokentiev <artinnok@mail.ru>
2016-10-01 13:35:53 +0300

    ADD cookiecutter + refactoring

--- {{ cookiecutter.name }}_project/{{ cookiecutter.name }}/config/urls/local.py
+++ {{ cookiecutter.name }}_project/{{ cookiecutter.name }}/config/urls/local.py
@@ -1,10 +1,14 @@
-from config.urls.base import *
-from core.views import error404
-
 from django.conf import settings
 from django.conf.urls.static import static
 
+from config.urls.base import *
+from core.views import error404
+
 
 urlpatterns = urlpatterns + [
-    url(r'^error404/$', error404),
+    url(
+        regex=r'^error404/$',
+        view=error404,
+        name='error404'
+    ),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
