8a0507386de414056a360ac7d81e57368e3a25b4
Artem Innokentiev <artinnok@users.noreply.github.com>
2017-04-02 17:12:26 +0300

    Update local.py

--- {{ cookiecutter.name }}_project/{{ cookiecutter.name }}/config/urls/local.py
+++ {{ cookiecutter.name }}_project/{{ cookiecutter.name }}/config/urls/local.py
@@ -1,5 +1,8 @@
 from django.conf import settings
 from django.conf.urls.static import static
+from django.conf.urls import include
+
+import debug_toolbar
 
 from config.urls.base import *
 from core.views import error404
@@ -10,5 +13,9 @@ urlpatterns = urlpatterns + [
         regex=r'^error404/$',
         view=error404,
         name='error404'
+    ),
+    url(
+        regex=r'^__debug__/',
+        view=include(debug_toolbar.urls)
     ),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
